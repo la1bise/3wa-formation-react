744c866c70c02050782c991abb01beda01f799d4
Tim <timothyroche1995@icloud.com>
2023-11-27 14:22:58 +0100

    conception finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import gon from "./assets/images/gon-image.png";
+import data from "./assets/data";
+import Carte from "./components/Carte";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-slate-700 font-mono ">
+      <ul className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 justify-center items-star ">
+        {data.map((item, index) => (
+          <li key={index} >
+            <Carte
+              frame={item.image}
+              name={item.name}
+              description={item.description}
+            ></Carte>
+          </li>
+        ))}
+       
+      </ul>
     </div>
   );
 }
---
+++ src/components/Carte.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Carte(props) {
+  return (
+    <div className='bg-white max-w-xs rounded-xl'>
+          <img 
+          className='rounded-xl '
+          src={props.frame} alt={`nom de ${props.name}`}/>
+          <h2 className='m-3 text-xl font-extrabold'>{props.name}</h2>
+          <p className='m-l-3 font-normal font-mono'>{props.description}</p>
+          <button className='bg-blue-700 m-2 p-2 rounded-xl'>read more</button>
+
+    </div>
+  )
+}
