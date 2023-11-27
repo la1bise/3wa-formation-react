257517967f6fdac65cfdb1f72ab2e2404a59c9a0
Tim <timothyroche1995@icloud.com>
2023-11-27 15:33:15 +0100

    1.0.1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import Notes from "./components.js/Notes";
 
 
 function App() {
+  const [ chiffre, setChiffre ] = useState (chiffre)
+
+  const setChiffre = [1,2,3,4,5]
+
   return (
     <div>
-     <h1>How did we do ?</h1>
-     <p>Please let us know how we did with your support request? All feedback is appreciated to help us improve our offering!</p>
+      <section>
+        
+        <h1>How did we do ?</h1>
+        <p>Please let us know how we did with your support request? All feedback is appreciated to help us improve our offering!</p>
 
      <Notes>
       
      </Notes>
+      </section>
     </div>
 
 
