fd874c14ffa2113d26dfb50ed786dba5dab04790
Tim <timothyroche1995@icloud.com>
2023-11-28 09:18:33 +0100

    reapet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import gon from "./assets/images/gon-image.png";
 import data from "./assets/data";
 import Carte from "./components/Carte";
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-700 font-mono ">
-      <ul className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 justify-center items-star ">
-        {data.map((item, index) => (
+       <ul className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 justify-center items-star ">
+         {data.map((item, index) => (
           <li key={index} >
             <Carte
               frame={item.image}
@@ -14,9 +13,9 @@ function App() {
               description={item.description}
             ></Carte>
           </li>
-        ))}
-       
+        ))}   
       </ul>
+     
     </div>
   );
 }
--- src/components/Carte.js
+++ src/components/Carte.js
@@ -5,7 +5,8 @@ export default function Carte(props) {
     <div className='bg-white max-w-xs rounded-xl'>
           <img 
           className='rounded-xl '
-          src={props.frame} alt={`nom de ${props.name}`}/>
+          src={props.frame} 
+          alt={props.name}/>
           <h2 className='m-3 text-xl font-extrabold'>{props.name}</h2>
           <p className='m-l-3 font-normal font-mono'>{props.description}</p>
           <button className='bg-blue-700 m-2 p-2 rounded-xl'>read more</button>
