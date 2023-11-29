704f440cdfd569d63414be664060f1bbc3ef781d
Tim <timothyroche1995@icloud.com>
2023-11-29 10:04:39 +0100

    app done

--- src/components/Taskitem.js
+++ src/components/Taskitem.js
@@ -1,12 +1,10 @@
-import { GoTrash } from "react-icons/go";
+import { FcFullTrash } from 'react-icons/fc';
 
-
-export default function TaskItem(props){
-    return (
-        <li
-              className="flex items-center justify-between px-8 bg-slate-100 text-center py-4 rounded shadow shadow-slate-400">
-              {props.name}
-              <GoTrash onClick={props.removeItem} className="w-6 h-6"/>
-            </li>
-    );
+export default function TaskItem(props) {
+  return (
+    <li className="flex items-center justify-between px-8 bg-slate-100 text-center py-4 rounded shadow shadow-slate-400">
+      {props.name}
+      <FcFullTrash onClick={props.removeItem} className="w-6 h-6" />
+    </li>
+  );
 }
