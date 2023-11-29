cb5bdf672a3cd66b5a15246b0b56498dcc6e4dd1
Tim <timothyroche1995@icloud.com>
2023-11-29 16:08:00 +0100

    1.0.2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,66 @@
 import data from "./assets/data";
 import Carte from "./components/Carte";
+import { useReducer } from "react"; //#1
+
+
+
+
+const initialValue = {//#3
+    textEntered: "",
+    deckFilter: null,
+  };
+
+
+const reducer = function (state, action) { //#2
+  switch (action.type) {
+    case "onChangeInput": // if(action.type === 'onChangeInput')
+      const searchFilter = data.filter((item) => //#7
+        item.name.toLowerCase().includes(action.payload.toLowerCase)
+      );
+      return { //#8
+        ...state,
+        textEntered: action.payload,
+        deckFilter: searchFilter,
+      };
+
+    default:
+      break;
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialValue); //#4
+
+  
+
+  const onChangeHandler = (event) =>
+    dispatch({ type: "onChangeInput", payload: event.target.value }); //#5
+  
+    // short circuiting => permet d'évaluer les valeurs données et va choissir la premiere valeur VERIDIQUE
+  const decks = state.deckFilter || data; //#9
+
+
   return (
     <div className="min-h-screen bg-slate-700 font-mono ">
-       <ul className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 justify-center items-star ">
-         {data.map((item, index) => (
-          <li key={index} >
+      <input
+        onChange={onChangeHandler} //#6
+        className="rounded-lg m-2 py-1 text-center"
+        type="search"
+        placeholder="search"
+      />
+
+      <ul className="grid sm:grid-cols-2 md:grid-cols-3 gap-3 justify-center items-star ">
+        {decks.map((item, index) => (
+          <li 
+          key={index}>
             <Carte
               frame={item.image}
               name={item.name}
               description={item.description}
             ></Carte>
           </li>
-        ))}   
+        ))}
       </ul>
-     
     </div>
   );
 }
--- src/components/Carte.js
+++ src/components/Carte.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaArrowRight } from "react-icons/fa";
 
 export default function Carte(props) {
   return (
@@ -8,8 +9,10 @@ export default function Carte(props) {
           src={props.frame} 
           alt={props.name}/>
           <h2 className='m-3 text-xl font-extrabold'>{props.name}</h2>
-          <p className='m-l-3 font-normal font-mono'>{props.description}</p>
-          <button className='bg-blue-700 m-2 p-2 rounded-xl'>read more</button>
+          <p className='m-3 font-normal font-mono'>{props.description}</p>
+          
+          <button className='flex items-center justify-center gap-1 bg-blue-700 m-4 p-2 rounded-xl w-48'>read more <FaArrowRight className='' /></button>
+          
 
     </div>
   )
