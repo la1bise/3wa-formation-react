fac6a557900daee08baef39c45fb2231f10cba6d
Tim <timothyroche1995@icloud.com>
2023-11-30 13:39:37 +0100

    nav barrereact

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import BlogPage from './pages/BlogPage';
+import AboutPage from './pages/AboutPage';
+import CreateBlog from './pages/CreateBlog';
+import DetailBlog from './pages/DetailBlog';
+import Header from './components/Header';
 
-function App() {
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <Routes>
+      {/* 
+      `Route` permet de définir un chemin et le composant à afficher sur ce dernier
+      Dans la version v5 de `react-router-dom` element était nommé `component`
+    */}
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage/>} />
+        {/* 
+        Les chemin imbriquéespermette d'avoir des chemins qui vont s'imbriquer par rapport à leurs chemins principales
+        /blogs => Page avec tous les articles du site
+        /blogs/1 accéder a un article à partir du chemin '/blogs'
+        /blogs/new =< un formulaire pour crée un blog
+        /blog-create
+      */}
+      <Route path='/blogs'>
+        {/* 
+          La propriété `index` permet de définir le composant principal
+        */}
+        <Route index element={<BlogPage/>}/>
+        {/* 
+          Quand on utilise pour le chemin des liens 'enfant' juste le nom sans '/' devant
+        */}
+        <Route path='new' element={<CreateBlog/>}/>
+        {/* 
+          Les chemins dynamique permettent d'accéder à une page de façon automatique
+        */}
+        <Route path=':id' element={<DetailBlog/>}/>
+
+      </Route>
+    </Routes>
+  </>
   );
 }
-
-export default App;
