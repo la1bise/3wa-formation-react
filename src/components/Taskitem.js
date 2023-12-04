1a3a4411cf9427e01a50dbb003f5e1f6dec9fc8f
Tim <timothyroche1995@icloud.com>
2023-12-04 13:49:01 +0100

    finish

--- src/components/Taskitem.js
+++ src/components/Taskitem.js
@@ -1,6 +1,7 @@
 import { FcFullTrash } from 'react-icons/fc';
 
 export default function TaskItem(props) {
+  
   return (
     <li className="flex items-center justify-between px-8 bg-slate-100 text-center py-4 rounded shadow shadow-slate-400">
       {props.name}
